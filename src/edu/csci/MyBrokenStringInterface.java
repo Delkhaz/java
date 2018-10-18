6fd164afedf962cfba7b74bd588f20a78d251cc5
Delkhaz <Delkhazibrahimi@gmail.com>
2018-10-17 21:48:13 -0500

    src code

---
+++ src/edu/csci/MyBrokenStringInterface.java
@@ -0,0 +1,15 @@
+package edu.csci;
+
+public interface MyBrokenStringInterface {
+    void setBrokenString(String string);
+    String getBrokenString();
+    int countDigits();
+    StringBuilder getEvenCharacters();
+    StringBuilder getOddCharacters();
+    StringBuilder numbersDroppedString();
+    StringBuffer commasDroppedString();
+    StringBuilder spacesDroppedString();
+    String sortedAscendingString();
+    String sortedDescendingString();
+    String getReverseString();
+}
